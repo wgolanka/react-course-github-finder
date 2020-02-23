09d3b5088e7d27ff051fb4a38bda5c07190a745b
wgolanka <wiktoria.golanka@gmail.com>
2020-02-23 16:16:24 +0100

    Turned UserItem class into functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,47 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class UserItem extends Component {
+const UserItem = (props) => {
 
-    //possible way to add state but not recommended unless we need it for smth else too
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         id: 'id',
-    //         login: 'simonjefford',
-    //         avatar_url: 'https://avatars2.githubusercontent.com/u/136?v=4',
-    //         html_url: 'https://github.com/simonjefford'
-    //     }
-    // }
+    const {login, avatar_url, html_url} = props.user;
 
-    //other way is just to put state like that in the class
-    state = {
-        id: 'id',
-        login: 'simonjefford',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/136?v=4',
-        html_url: 'https://github.com/simonjefford'
-    };
+    return (
+        <div className="card text-center">
+            <img
+                src={avatar_url}
+                alt=""
+                className="round-img"
+                style={{width: '60px'}}
+            />
+            <h3>{login}</h3>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">
+                    More
+                </a>
 
-    render() {
-        const {login, avatar_url, html_url} = this.props.user;
-
-        return (
-            <div className="card text-center">
-                <img
-                    src={avatar_url}
-                    alt=""
-                    className="round-img"
-                    style={{width: '60px'}}
-                />
-                <h3>{login}</h3>
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">
-                        More
-                    </a>
-
-                </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+
+};
 
 export default UserItem;

b76a033627e072e3745699dfa417690e28cc6bc6
wgolanka <wiktoria.golanka@gmail.com>
2020-02-23 16:25:59 +0100

    Added propTypes to UserItem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const UserItem = ({user: {login, avatar_url, html_url}}) => {
 
@@ -20,6 +21,10 @@ const UserItem = ({user: {login, avatar_url, html_url}}) => {
         </div>
     );
 
+};
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
 };
 
 export default UserItem;

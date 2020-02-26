520e4af69616cde96fe8319e339b44749234a560
wgolanka <wiktoria.golanka@gmail.com>
2020-02-26 07:49:45 +0100

    Add search and button to submit search

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import axios from 'axios'
+import Search from "./components/users/Search";
 
 class App extends Component {
 
@@ -28,6 +29,7 @@ class App extends Component {
         return (
             <div className="App">
                 <Navbar/>
+                <Search/>
                 <div className="container">
                     <Users loading={this.state.loading} users={this.state.users}/>
                 </div>

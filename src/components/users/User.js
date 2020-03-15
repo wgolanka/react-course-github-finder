044fa53721c526e4de8f38956d2297c439c6af5f
golankaw <wiktoria.golanka@vimn.com>
2020-03-15 19:34:01 +0100

    Add bio, link to github profile and else to User UI

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -23,11 +23,13 @@ class User extends Component {
             bio,
             blog,
             login,
+            company,
             followers,
             following,
             public_repos,
             public_gists,
-            hireable
+            hireable,
+            html_url
         } = this.props.user;
 
         const {loading} = this.props;
@@ -46,9 +48,52 @@ class User extends Component {
                 {hireable ? <i className="fas fa-check text-success"/>
                     : <i className="fas fa-times-circle text-danger"/>
                 }
-                <div className=".card.grid-2">
-                    <img src={avatar_url} className="round-img" alt=""
-                         style={{width: '150px'}}/>
+                <div className='card grid-2'>
+
+                    <div className='all-center'>
+                        <img src={avatar_url}
+                             className="round-img"
+                             alt=""
+                             style={{width: '150px'}}
+                        />
+                        <h1>{name}</h1>
+                        <p>Location: {location}</p>
+                    </div>
+
+                    <div>
+                        {bio && <Fragment>
+                            <h3> Bio
+                                <p>
+                                    {bio}
+                                </p>
+                            </h3>
+                        </Fragment>}
+
+                        <a href={html_url} className="btn btn-dark my-1">
+                            Visit Github Profile
+                        </a>
+
+                        <ul>
+                            <li>
+                                {login &&
+                                <Fragment>
+                                    <strong>Username:</strong> {login}
+                                </Fragment>}
+                            </li>
+                            <li>
+                                {company &&
+                                <Fragment>
+                                    <strong>Company:</strong> {company}
+                                </Fragment>}
+                            </li>
+                            <li>
+                                {blog &&
+                                <Fragment>
+                                    <strong>Website:</strong> {blog}
+                                </Fragment>}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </Fragment>
         );

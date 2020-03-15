3c3b227b54d24c4e4f75172fd6e9abe6ea2ee153
wgolanka <wiktoria.golanka@gmail.com>
2020-03-15 17:57:32 +0100

    Add router to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import axios from 'axios'
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
 
@@ -51,20 +52,28 @@ class App extends Component {
     render() {
         const {users, loading} = this.state;
         return (
-            <div className="App">
-                <Navbar/>
-                <div className="container">
-                    <Alert alert={this.state.alert} />
-                    <Search searchUsers={this.searchUsers}
-                            clearUsers={this.clearUsers}
-                            showClearButton={
-                                users.length > 0
-                            }
-                            setAlert={this.setAlert}
-                    />
-                    <Users loading={loading} users={users}/>
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                    <div className="container">
+                        <Alert alert={this.state.alert}/>
+                        <Switch>
+                            <Route exact path='/' redner={props => (
+                                <Fragment>
+                                    <Search searchUsers={this.searchUsers}
+                                            clearUsers={this.clearUsers}
+                                            showClearButton={
+                                                users.length > 0
+                                            }
+                                            setAlert={this.setAlert}
+                                    />
+                                    <Users loading={loading} users={users}/>
+                                </Fragment>
+                            )} />
+                        </Switch>
+                    </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
